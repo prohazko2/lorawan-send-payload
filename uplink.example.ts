a9d896d2a3043a58851e10eaf2db0e99ef994715
Oleg Prohazko <prohazko@rightech.io>
2025-12-08 13:26:37 +0300

    example payload - sin temp and hum

--- uplink.example.ts
+++ uplink.example.ts
@@ -1,9 +1,20 @@
 import type { UplinkMessage } from "./src/types.ts";
 import { config } from "./src/config.ts";
+import { deviceState } from "./src/device-state.ts";
+
+function sin(min: number, max: number, speed = 0.01) {
+  const t = (Date.now() / 1000) * speed;
+  const amplitude = (max - min) / 2;
+  const offset = min + amplitude;
+  return +(offset + Math.sin(t) * amplitude).toFixed(0);
+}
 
 export function generateUplink(): UplinkMessage {
+  const t = sin(-40, 40);
+  const h = sin(20, 80);
+
   return {
     fPort: config.uplinkFPort,
-    payload: Buffer.from(`${new Date().toISOString()}`),
+    payload: Buffer.from([t, h]),
   };
 }
